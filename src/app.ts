1d76ee29dddd5f85f5f12368a01cbb42c6bc00a7
domdom <dom.dumont@gmail.com>
2017-05-20 13:04:20 +0200

    plein de choses, toujours le tuto

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,7 @@ function onListening() {
     ? "pipe " + addr
     : "port " + addr.port;
   console.log("Listening on " + bind);
+  //Test
 }
 
 
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import * as express from "express";
+import * as path from "path";
+import { IndexRoute } from "./routes/index";
 
 export class Server {
   public app: express.Application;
@@ -15,6 +17,7 @@ export class Server {
 
     //add api
     this.api();
+    //Test
   }
 
   /**
@@ -28,12 +31,27 @@ export class Server {
   }
 
 
-  public config() {
-    //empty for now
+  public config() 
+  {
+    //add static paths
+    this.app.use(express.static(path.join(__dirname, "public")));
+
+    //configure pug
+    this.app.set("views", path.join(__dirname, "views"));
+    this.app.set("view engine", "pug");
+
   }
 
 
   public routes() {
     //empty for now
+     let router: express.Router;
+    router = express.Router();
+
+    //IndexRoute
+    IndexRoute.create(router);
+
+    //use router middleware
+    this.app.use(router);
   }
 }
